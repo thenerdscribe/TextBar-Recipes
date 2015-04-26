8210827d61d975e24cc4048495e86a7d6e0e0816
Ryan Morton <morton.ryan92@gmail.com>
2015-04-26 10:05:50 -0700

    Messing with recipes

---
+++ external.sh
@@ -0,0 +1,2 @@
+CURL=`curl http://ipinfo.io/ip`
+echo "<html><font face=\"helveticaneue-ultralight\">" $CURL"</font></html>"
--- ip.sh
+++ ip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-IP=`ipconfig getifaddr en0`
+IP=`ipconfig getifaddr en1`
 echo "<html><font face=\"helveticaneue-ultralight\">" $IP"</font></html>"
