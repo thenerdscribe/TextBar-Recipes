9aa3eb946350ea4110201ee83c944919d416101f
Ryan Morton <morton.ryan92@gmail.com>
2015-04-26 10:36:53 -0700

    Changed to Light Font

--- external.sh
+++ external.sh
@@ -1,2 +1,2 @@
 CURL=`curl http://ipinfo.io/ip`
-echo "<html><font face=\"helveticaneue-ultralight\">" $CURL"</font></html>"
+echo "<html><font face=\"helveticaneue-light\">" $CURL"</font></html>"
---
+++ rdio.sh
@@ -0,0 +1,10 @@
+read -r running <<<"$(ps -ef | grep \"MacOS/Rdio\" | grep -v \"grep\" | wc -l)" &&
+test $running != 0 &&
+IFS='|' read -r theArtist theName <<<"$(osascript <<<'tell application "Rdio"
+        tell current track
+          set theArtist to artist
+          set theName to name
+          return theArtist & "|" & theName
+        end tell
+    end tell')" &&
+echo "<html><font face=\"helveticaneue-light\">$theArtist - $theName</font></html>"
